3349c3f0c360c0fc3d5c896332512ab6d8075c11
Michael Yu <yuanchao.m.yu@gmail.com>
2022-02-28 20:10:24 -0500

    Change back to peter as it was failing the tests (#4)
    * Change back to peter as it was failing the tests
    
    * Change back to peter as it was failing the tests

--- services/ui/test/ui_test.exs
+++ services/ui/test/ui_test.exs
@@ -4,9 +4,8 @@ defmodule UiTest do
 
   import Mock
 
-
   @users [
-    %{id: "1", name: "Michael"},
+    %{id: "1", name: "Peter"},
     %{id: "2", name: "John"},
     %{id: "3", name: "Simon"}
   ]
